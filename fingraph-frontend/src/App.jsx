3aa29065f987d409f2ed8d496ce2b3bf4bf3498b
nikhil <nikhilshinde9992@gmail.com>
2026-08-11 15:36:43 +0530

    feat: initialize FinGraph frontend foundation

---
+++ fingraph-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>FinGraph</h1>
+      <p>Real-Time Fraud Syndicate Analytics</p>
+      <span>Frontend & UI Development</span>
+    </div>
+  );
+}
+export default App;
