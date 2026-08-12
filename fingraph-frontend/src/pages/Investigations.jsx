331dd93e3e1875cf7d607b8b6e8a932f129fe89c
nikhil <nikhilshinde9992@gmail.com>
2026-08-12 17:22:14 +0530

    feat:complete fraud analytics investigations page

---
+++ fingraph-frontend/src/pages/Investigations.jsx
@@ -0,0 +1,10 @@
+function Investigations() {
+  return (
+    <div>
+      <h2>Investigations</h2>
+      <p>Investigate suspicious financial transactions and fraud cases.</p>
+    </div>
+  );
+}
+
+export default Investigations;
