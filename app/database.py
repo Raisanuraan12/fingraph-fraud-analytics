c86af03aa4414c3556088b0c700646aab3f82953
shubhamgawari9226 <shubhamgawari64@gmail.com>
2026-08-27 00:06:03 +0530

    feat(infra): build Neo4j Python driver connection pool and lifecycle manager

--- app/database.py
+++ app/database.py
@@ -4,9 +4,9 @@ from neo4j import GraphDatabase
 
 load_dotenv()
 
-URI = os.getenv("NEO4J_URI")
-USERNAME = os.getenv("NEO4J_USERNAME")
-PASSWORD = os.getenv("NEO4J_PASSWORD")
+URI = os.getenv('NEO4J_URI', 'neo4j://127.0.0.1:7687')
+USERNAME = os.getenv('NEO4J_USERNAME', 'neo4j')
+PASSWORD = os.getenv('NEO4J_PASSWORD', 'neo4j')
 
 driver = GraphDatabase.driver(
     URI,
